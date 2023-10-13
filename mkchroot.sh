bc3b4c80afe0787eee9610f6e57fd73149c80c05
Stephane Fontaine <esselfe16@gmail.com>
2023-10-13 09:33:36 -0400

    work on su and passwd

--- mkchroot.sh
+++ mkchroot.sh
@@ -3,35 +3,86 @@
 mkdir chroot
 cd chroot
 
-ln -sfv lib lib64
-cp -rpdv /lib lib | sed '/\/lib\/modules/d;/\/lib\/firmware/d'
-cp -rpdv /bin bin
+mkdir -v lib
 mkdir -pv usr/lib
+ln -sfv lib lib64
+cp -rpdv /lib/* usr/lib | sed '/\/lib\/modules/d;/\/lib\/firmware/d'
+mv -v usr/lib/ld-linux* lib
+
+cp -rpdv /bin bin
 ln -sfv lib usr/lib64
 
-rm -rf lib/firmware
-rm -rf lib/modules/*
+rm -rf usr/lib/firmware
+rm -rf usr/lib/modules/*
 cp -pdv /usr/lib/libc.* usr/lib
 cp -pdv /usr/lib/libm.* usr/lib
 cp -v /usr/lib/libcap.so.2 usr/lib
 cp -v /usr/lib/libmpfr.so.6 usr/lib
 cp -v /usr/lib/libgmp.so.10 usr/lib
-ln -sv ../../lib/libncurses.so.6 usr/lib/libncurses.so.6
+cp -v /usr/lib/libacl.so.1 usr/lib
+cp -v /usr/lib/libattr.so.1 usr/lib
+cp -v /usr/lib/libcrack.so.2 usr/lib
+cp -v /usr/lib/libnss_systemd.so.2 usr/lib
+cp -v /usr/lib/libcrypt.so.1 usr/lib
+
+mkdir -v usr/share
+cp -rpv /usr/share/cracklib usr/share
 
 cp -fv /usr/bin/ls bin
 rm -fv bin/{echo,env,grep,gawk,ln,pwd}
 cp -fv /usr/bin/{cat,cut,date,df,du,echo,env,grep,gawk,ln,pwd,tr,which} bin
+cp -fv /usr/bin/{dircolors,useradd,userdel,groupadd,groupdel,mkdir,passwd} bin
+cp -fv /usr/bin/{grpconv,pwconv,[,basename,dirname,false,true,uniq,test} bin
+cp -fv /usr/bin/{head,tail,install,md5sum,sha1sum,sha224sum,sha256sum} bin
+cp -fv /usr/bin/{sha384sum,sha512sum,shuf,sleep,sort,stat,touch,whoami} bin
+
+rm -v bin/{bashbug,dmesg,findmnt,kmod,login,lsfd,lsh,lsmod,lvis,lvu,more}
+rm -v bin/{mount,mountpoint,umount,wdctl}
 
 mkdir -v etc
 cp -v /etc/ld.so.conf etc
+mkdir etc/ld.so.conf.d
 cp -pv /etc/inputrc etc
+cp -pv /etc/login.defs etc
+cp -pv /etc/nsswitch.conf etc
+cp -rpdv /etc/pam.d etc
+cp -rpv /etc/security etc
 echo 'export PATH=/bin:/usr/bin' > etc/profile
 echo 'export PS1="\t\u@\l:\w\\$ "' >> etc/profile
 echo 'export LANG=en_US.UTF-8' >> etc/profile
+echo -e "export LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:"\
+"do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:"\
+"ca=00:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:"\
+"*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:"\
+"*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:"\
+"*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:"\
+"*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:"\
+"*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:"\
+"*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:"\
+"*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:"\
+"*.dwm=01;31:*.esd=01;31:*.avif=01;35:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:"\
+"*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:"\
+"*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:"\
+"*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:"\
+"*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.webp=01;35:*.ogm=01;35:"\
+"*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:"\
+"*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:"\
+"*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:"\
+"*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:"\
+"*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:"\
+"*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:"\
+"*.opus=00;36:*.spx=00;36:*.xspf=00;36:*~=00;90:*#=00;90:*.bak=00;90:"\
+"*.old=00;90:*.orig=00;90:*.part=00;90:*.rej=00;90:*.swp=00;90:*.tmp=00;90:"\
+"*.dpkg-dist=00;90:*.dpkg-old=00;90:*.ucf-dist=00;90:*.ucf-new=00;90:"\
+"*.ucf-old=00;90:*.rpmnew=00;90:*.rpmorig=00;90:*.rpmsave=00;90:'"\
+	>> etc/profile
 echo 'alias ls="ls -A --color=auto"' >> etc/profile
 echo 'alias qw="exit"' >> etc/profile
 
 echo 'root:x:0:0:root:/root:/bin/bash' > etc/passwd
 echo 'root:x:0:root' > etc/group
+echo 'mail:x:8:' >> etc/group
 mkdir -v root
+mkdir -pv var/spool/mail
+chgrp -v mail var/spool/mail
 
